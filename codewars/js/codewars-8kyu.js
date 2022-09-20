c3cbff623817f8267fe2d7f7a167276da6a5b272
Roman Panshin <panshin13@gmail.com>
2022-09-20 11:20:31 +0300

    JS | codewars 8kyu kata commit

---
+++ codewars/js/codewars-8kyu.js
@@ -0,0 +1,12 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+//
+// Example : n=5 --> [5,4,3,2,1]
+
+
+const reverseSeq = n => {
+    let res = []
+    for (let i = 1; i <= n; i++) {
+        res.unshift(i)
+    }
+    return res;
+};
